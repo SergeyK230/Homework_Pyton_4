3a75a8ac2dae7c38f91ac169a8c48615cf23d495
sergey.k <k.sergey2010@yandex.ru>
2022-07-22 18:12:42 +0700

    zadacha_4 done

--- zadacha_4.py
+++ zadacha_4.py
@@ -5,4 +5,12 @@
 # 
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
+from random import randint
+
+
 k = int(input('Введите число -> '))
+a = randint(0,100)
+b = randint(0,100)
+c = randint(0,100)
+with open('E:\Документы\GeekBrains\Знакомство c Python\homework_4\zadacha_4.txt', 'w', encoding='utf-8') as file:
+   file.write(f'{a}*x^{k} + {b}*x + {c} = 0')
